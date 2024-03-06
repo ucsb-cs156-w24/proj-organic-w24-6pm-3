c8beb045755b3f40638400845f68266640aec8f0
Kenisha Vaswani <kenishavaswani@Kenishas-MacBook-Pro.local>
2024-03-06 00:30:37 -0800

    kv - fixed endpoints

--- frontend/src/main/pages/StaffIndexPage.js
+++ frontend/src/main/pages/StaffIndexPage.js
@@ -27,9 +27,9 @@ export default function StaffIndexPage() {
   const { data: staff, error: _error, status: _status } =
     useBackend(
       // Stryker disable next-line all : don't test internal caching of React Query
-      ["/api/courses/staff"],
+      ["/api/courses/{courseId}/staff"],
       // Stryker disable next-line all : GET is the default
-      { method: "GET", url: "/api/courses/staff" },
+      { method: "GET", url: "/api/courses/{courseId}/staff" },
       []
     );
 
---
+++ frontend/src/tests/pages/StaffIndexPage.test.js
@@ -0,0 +1,251 @@
+import {  render, waitFor, screen } from "@testing-library/react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { MemoryRouter } from "react-router-dom";
+import StaffIndexPage from "main/pages/StaffIndexPage";
+
+
+import { apiCurrentUserFixtures } from "fixtures/currentUserFixtures";
+import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
+import { staffFixtures } from "fixtures/staffFixtures";
+import axios from "axios";
+import AxiosMockAdapter from "axios-mock-adapter";
+import mockConsole from "jest-mock-console";
+
+
+const mockToast = jest.fn();
+jest.mock('react-toastify', () => {
+    const originalModule = jest.requireActual('react-toastify');
+    return {
+        __esModule: true,
+        ...originalModule,
+        toast: (x) => mockToast(x)
+    };
+});
+
+describe("StaffIndexPage tests", () => {
+
+    const axiosMock = new AxiosMockAdapter(axios);
+
+    const testId = "StaffTable";
+
+    const setupAdminUser = () => {
+        axiosMock.reset();
+        axiosMock.resetHistory();
+        axiosMock.onGet("/api/currentUser").reply(200, apiCurrentUserFixtures.adminUser);
+        axiosMock.onGet("/api/systemInfo").reply(200, systemInfoFixtures.showingNeither);
+    };
+
+    const setupInstructorUser = () => {
+        axiosMock.reset();
+        axiosMock.resetHistory();
+        axiosMock.onGet("/api/currentUser").reply(200, apiCurrentUserFixtures.instructorUser);
+        axiosMock.onGet("/api/systemInfo").reply(200, systemInfoFixtures.showingNeither);
+    }
+
+    // test("Renders with Delete Button for admin user", async () => {
+    //     // arrange
+    //     setupAdminUser();
+    //     const queryClient = new QueryClient();
+    //     axiosMock.onGet("/api/courses/{courseId}/staff").reply(200, []);
+
+    //     // act
+    //     render(
+    //         <QueryClientProvider client={queryClient}>
+    //             <MemoryRouter>
+    //                 <StaffIndexPage />
+    //             </MemoryRouter>
+    //         </QueryClientProvider>
+    //     );
+
+    //     // assert
+    //     await waitFor( ()=>{
+    //         expect(screen.getByText(/Delete Staff/)).toBeInTheDocument();
+    //     });
+    //     const button = screen.getByText(/Create Course/);
+    //     expect(button).toHaveAttribute("href", "/courses/create");
+    //     expect(button).toHaveAttribute("style", "float: right;");
+    // });
+
+    // test("Renders with Create Button for instructor user", async () => {
+    //     // arrange
+    //     setupInstructorUser();
+    //     const queryClient = new QueryClient();
+    //     axiosMock.onGet("/api/courses/{courseId}/staff").reply(200, []);
+
+    //     // act
+    //     render(
+    //         <QueryClientProvider client={queryClient}>
+    //             <MemoryRouter>
+    //                 <CourseIndexPage />
+    //             </MemoryRouter>
+    //         </QueryClientProvider>
+    //     );
+
+    //     // assert
+    //     await waitFor( ()=>{
+    //         expect(screen.getByText(/Create Course/)).toBeInTheDocument();
+    //     });
+    //     const button = screen.getByText(/Create Course/);
+    //     expect(button).toHaveAttribute("href", "/courses/create");
+    //     expect(button).toHaveAttribute("style", "float: right;");
+    // });
+
+    test("renders three staff members correctly for admin", async () => {    
+        // arrange
+        setupAdminUser();
+        const queryClient = new QueryClient();
+        axiosMock.onGet("/api/courses/{courseId}/staff").reply(200, staffFixtures.threeStaff);
+
+        // act
+        render(
+            <QueryClientProvider client={queryClient}>
+                <MemoryRouter>
+                    <StaffIndexPage />
+                </MemoryRouter>
+            </QueryClientProvider>
+        );
+
+        // assert
+        await waitFor(() => { expect(screen.getByTestId(`${testId}-cell-row-0-col-id`)).toHaveTextContent("1"); });
+        expect(screen.getByTestId(`${testId}-cell-row-1-col-id`)).toHaveTextContent("2");
+        expect(screen.getByTestId(`${testId}-cell-row-2-col-id`)).toHaveTextContent("3");
+
+    });
+
+    test("renders three staff members correctly for instructor", async () => {      
+        // arrange
+        setupInstructorUser();
+        const queryClient = new QueryClient();
+        axiosMock.onGet("/api/courses/{courseId}/staff").reply(200, staffFixtures.threeStaff);
+
+        // act
+        render(
+            <QueryClientProvider client={queryClient}>
+                <MemoryRouter>
+                    <StaffIndexPage />
+                </MemoryRouter>
+            </QueryClientProvider>
+        );
+
+        // assert
+        await waitFor(() => { expect(screen.getByTestId(`${testId}-cell-row-0-col-id`)).toHaveTextContent("1"); });
+        expect(screen.getByTestId(`${testId}-cell-row-1-col-id`)).toHaveTextContent("2");
+        expect(screen.getByTestId(`${testId}-cell-row-2-col-id`)).toHaveTextContent("3");
+
+    });
+
+    test("renders empty table when backend unavailable, admin", async () => {
+        // arrange
+        setupAdminUser();
+        const queryClient = new QueryClient();
+        axiosMock.onGet("/api//{courseId}/staff").timeout();
+        const restoreConsole = mockConsole();
+
+        // act
+        render(
+            <QueryClientProvider client={queryClient}>
+                <MemoryRouter>
+                    <StaffIndexPage />
+                </MemoryRouter>
+            </QueryClientProvider>
+        );
+
+        // assert
+        await waitFor(() => { expect(axiosMock.history.get.length).toBeGreaterThanOrEqual(1); });
+
+        restoreConsole();
+
+        expect(screen.queryByTestId(`${testId}-cell-row-0-col-id`)).not.toBeInTheDocument();
+    });
+
+    test("renders empty table when backend unavailable, instructor", async () => {
+        // arrange
+        setupInstructorUser();
+        const queryClient = new QueryClient();
+        axiosMock.onGet("/api/courses/{courseId}/staff}").timeout();
+        const restoreConsole = mockConsole();
+
+        // act
+        render(
+            <QueryClientProvider client={queryClient}>
+                <MemoryRouter>
+                    <StaffIndexPage />
+                </MemoryRouter>
+            </QueryClientProvider>
+        );
+
+        // assert
+        await waitFor(() => { expect(axiosMock.history.get.length).toBeGreaterThanOrEqual(1); });
+
+        restoreConsole();
+
+        expect(screen.queryByTestId(`${testId}-cell-row-0-col-id`)).not.toBeInTheDocument();
+    });
+
+    // test("what happens when you click delete, admin", async () => {
+    //     // arrange
+    //     setupAdminUser();
+    //     const queryClient = new QueryClient();
+    //     axiosMock.onGet("/api/courses/{courseId}/staff").reply(200, staffFixtures.threeStaff);
+    //     // axiosMock.onDelete("/api/courses/delete").reply(200, "Course with id 1 was deleted");
+
+    //     // act
+    //     render(
+    //         <QueryClientProvider client={queryClient}>
+    //             <MemoryRouter>
+    //                 <StaffIndexPage />
+    //             </MemoryRouter>
+    //         </QueryClientProvider>
+    //     );
+
+    //     // assert
+    //     await waitFor(() => { expect(screen.getByTestId(`${testId}-cell-row-0-col-id`)).toBeInTheDocument(); });
+
+    //     expect(screen.getByTestId(`${testId}-cell-row-0-col-id`)).toHaveTextContent("1");
+
+    //     // const deleteButton = screen.getByTestId(`${testId}-cell-row-0-col-Delete-button`);
+    //     // expect(deleteButton).toBeInTheDocument();
+
+    //     // act
+    //     // fireEvent.click(deleteButton);
+
+    //     // assert
+    //     await waitFor(() => { expect(mockToast).toBeCalledWith("Course with id 1 was deleted") });
+
+    // });
+
+    // test("what happens when you click delete, instructor", async () => {
+    //     // arrange
+    //     setupInstructorUser();
+    //     const queryClient = new QueryClient();
+    //     axiosMock.onGet("/api/courses/all").reply(200, coursesFixtures.threeCourses);
+    //     axiosMock.onDelete("/api/courses/delete").reply(200, "Course with id 1 was deleted");
+
+    //     // act
+    //     render(
+    //         <QueryClientProvider client={queryClient}>
+    //             <MemoryRouter>
+    //                 <CourseIndexPage />
+    //             </MemoryRouter>
+    //         </QueryClientProvider>
+    //     );
+
+    //     // assert
+    //     await waitFor(() => { expect(screen.getByTestId(`${testId}-cell-row-0-col-id`)).toBeInTheDocument(); });
+
+    //     expect(screen.getByTestId(`${testId}-cell-row-0-col-id`)).toHaveTextContent("1");
+
+    //     const deleteButton = screen.getByTestId(`${testId}-cell-row-0-col-Delete-button`);
+    //     expect(deleteButton).toBeInTheDocument();
+
+    //     // act
+    //     fireEvent.click(deleteButton);
+
+    //     // assert
+    //     await waitFor(() => { expect(mockToast).toBeCalledWith("Course with id 1 was deleted") });
+
+    // });
+
+});
+
+
